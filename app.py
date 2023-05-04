c672a3b3981380b4e72ae50923f27932f989b3ce
jtliu <13927303506@163.com>
2023-05-04 17:54:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
-import os
+from flask import Flask, request, Response
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Welcome, this is a Flask app deployed on Zeabur"
+@app.route('/proxy/<path:match>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def proxy(match):
+    url = f'https://api.openai.com/{match}'
+    headers = {key: value for (key, value) in request.headers if key != 'Host'}
+    resp = requests.request(
+        method=request.method,
+        url=url,
+        headers=headers,
+        data=request.get_data(),
+        params=request.args,
+        cookies=request.cookies,
+        stream=True,
+    )
+
+    response = Response(resp.content, resp.status_code, resp.raw.headers.items())
+    return response
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
+    app.run(debug=True)
